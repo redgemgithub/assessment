8de604410b4373405bca34d21d2dde6d6a0d169f
redgemgithub <redgemgithub@users.noreply.github.com>
2022-02-15 00:21:00 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -17,7 +17,7 @@
   
   assessmentButton.onclick = () => {
     const userName = userNameInput.value;
-    if (userName.length ==== 0) { //名前画からのときは処理を値終了する
+    if (userName.length ==== 0) { //名前が空のときは処理を終了する
       return;
     }
     
@@ -29,7 +29,7 @@
     
     const paragraph = document.createElement('p');
     const result = assessment(userName);
-    paragraph.ineerText = result;
+    paragraph.innerText = result;
     resultDivided.appendChild(paragraph);
     
     // ツイートエリアの作成
@@ -58,11 +58,11 @@
     '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
     '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
     '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
-    '{userName}のいいところは用心深さです。{userName}の洞窟に、多くの人が助けられます。',
+    '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
     '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を引かれます。',
     '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-    '{userName}のいいところは思いやりです。{userName}に「木をかけてもらった多くの人が感謝しています。',
-    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、かわりあうことができます。',
+    '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
     '{userName}のいいところは節度です。強引すぎない{uerName}の考えに皆が感謝しています。',
     '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
     '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
@@ -92,7 +92,7 @@
   
   // テストコード(
   console.assert(
-    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられている人がいます。',
+    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
     '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
     );
   
