5d6bc87989f7ae8100cc2805937bc60aaf8fabee
redgemgithub <redgemgithub@users.noreply.github.com>
2022-02-15 00:58:31 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   const userNameInput = document.getElementById('user-name');
-  const assessmentButton = document.getElementByID('assessment');
+  const assessmentButton = document.getElementById('assessment');
   const resultDivided = document.getElementById('result-area');
   const tweetDivided = document.getElementById('tweet-area');
   
